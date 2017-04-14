465d6cbfdd636d6eab4fd8e2228b06aaa0af75c9
Scott <scottyla19@gmail.com>
2017-04-14 03:44:41 -0600

    first commit

---
+++ app/src/main/java/com/example/android/missoulahoodguide/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.android.missoulahoodguide;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        // Create an adapter that knows which fragment should be shown on each page
+        SimpleFragmentPagerAdapter adapter = new SimpleFragmentPagerAdapter(getSupportFragmentManager());
+
+        // Set the adapter onto the view pager
+        viewPager.setAdapter(adapter);
+    }
+}
